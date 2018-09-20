4fd77e08d73edaa9fd0015ffce7755950f97723b
Luoxzhg <luoxzhg@gmail.com>
2018-09-20 18:36:34 +0800

    update listview.py: fixed some bug and add __repr__

--- python/listview.py
+++ python/listview.py
@@ -1,4 +1,4 @@
-# _*_encoding: utf8 _*_
+# -*- coding: utf8 -*-
 """
 Used for reference a list or part without copy.
 Changes of the referenced object will reflect to the views.
@@ -6,7 +6,13 @@ View is immutable.
 """
 import numbers
 
-class listview(list):
+# helper functions
+def _calcindex(self, n):
+    return self.start + n * self.step 
+
+class listview:
+    __slots__ = ('start', 'stop', 'step', 'lst')
+    
     def __new__(cls, lst, start=None, stop=None, step=None):
         ln = len(lst)
         start, stop, step = slice(start, stop, step).indices(ln)
@@ -14,12 +20,12 @@ class listview(list):
             if (start, stop, step) == (0, ln, 1):
                 return lst
             
-            start = lst.start + start *lst.step
-            stop = lst.start + stop * lst.step
+            start = _calcindex(lst, start)  #lst.start + start *lst.step
+            stop = _calcindex(lst, stop)    #lst.start + stop * lst.step
             step = lst.step * step
             lst = lst.lst
             
-        self = object.__new__(listview)
+        self = object.__new__(cls)
         self.lst = lst
         self.start = start
         self.stop = stop
@@ -44,12 +50,21 @@ class listview(list):
 ##        self.lst = lst
 ##
 
+    def __repr__(self):
+        return 'listview([' + ', '.join(str(e) for e in self) + '])'
+
+    def __iter__(self):
+        return (self.lst[i] for i in range(self.start, self.stop, self.step))
+
+##    def __contains__(self, v):
+##        return v in iter(self)
+
     def __len__(self):
         return (self.stop - self.start) // self.step
     
     def __getitem__(self, n):
         if isinstance(n, numbers.Integral):
-            index = self.start + n * self.step
+            index = _calcindex(self, n)     #self.start + n * self.step
             if index >= self.stop:
                 raise IndexError("Index out of range")
             return self.lst[index]
@@ -58,4 +73,3 @@ class listview(list):
         else:
             raise TypeError("index must be int")
 
-    
