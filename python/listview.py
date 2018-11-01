1e168f8803e16648ab1c59bfbebdf0fd846d8bbd
Luoxzhg <luoxzhg@gmail.com>
2018-11-01 19:20:48 +0800

    update listview: using metaclass instead of __new__ and add doctest string

--- python/listview.py
+++ python/listview.py
@@ -3,6 +3,20 @@
 Used for reference a list or part without copy.
 Changes of the referenced object will reflect to the views.
 View is immutable.
+
+>>> l=[1,2,3,4]
+>>> v=listview(l)
+>>> v
+listview([1, 2, 3, 4])
+>>> v2=listview(v)
+>>> v2 is v
+True
+>>> v3=v[:2]
+>>> v3
+listview([1, 2])
+>>> v4=v[::2]
+>>> v4
+listview([1, 3])
 """
 import numbers
 
@@ -10,10 +24,8 @@ import numbers
 def _calcindex(self, n):
     return self.start + n * self.step 
 
-class listview:
-    __slots__ = ('start', 'stop', 'step', 'lst')
-    
-    def __new__(cls, lst, start=None, stop=None, step=None):
+class _meta(type):
+    def __call__(cls, lst, start=None, stop=None, step=None):
         ln = len(lst)
         start, stop, step = slice(start, stop, step).indices(ln)
         if isinstance(lst, cls):
@@ -25,39 +37,22 @@ class listview:
             step = lst.step * step
             lst = lst.lst
             
-        self = object.__new__(cls)
+        return type.__call__(cls, lst, start, stop, step)
+
+class listview(metaclass=_meta):
+    __slots__ = ('lst', 'start', 'stop', 'step')
+    
+    def __init__(self, lst, start=None, stop=None, step=None):
         self.lst = lst
         self.start = start
         self.stop = stop
         self.step = step
-
-        return self
-    
-##    def __init__(self, lst, start=None, stop=None, step=None):
-##        print("----init-----")
-##        start, stop, step = slice(start, stop, step).indices(len(lst))
-##        
-##        if isinstance(lst, listview):
-##            # todo: 如果 start, stop, step 全为 None，则应该返回 lst，需要在元类中实现
-##            start = lst.start + start *lst.step
-##            stop = lst.start + stop * lst.step
-##            step = lst.step * step
-##            lst = lst.lst
-##
-##        self.start = start
-##        self.stop = stop
-##        self.step = step
-##        self.lst = lst
-##
 
     def __repr__(self):
         return 'listview([' + ', '.join(str(e) for e in self) + '])'
 
     def __iter__(self):
         return (self.lst[i] for i in range(self.start, self.stop, self.step))
-
-##    def __contains__(self, v):
-##        return v in iter(self)
 
     def __len__(self):
         return (self.stop - self.start) // self.step
@@ -73,3 +68,6 @@ class listview:
         else:
             raise TypeError("index must be int")
 
+if __name__=='__main__':
+    import doctest
+    doctest.testmod(verbose=True)
